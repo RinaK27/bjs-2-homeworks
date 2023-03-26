cafc49e1a5e50396a25a8e0a5f71204340d284d3
RinaK27 <megustarina@gmail.com>
2023-03-26 21:20:08 +0400

    Реализовано первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,14 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
-}
-
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+  const d = Math.pow(b, 2) - 4 * a * c; // вычисляем дискриминант
+  if (d < 0) {
+    return []; // дискриминант меньше нуля, корней нет
+} else if (d === 0) {
+  const x = -b / (2 * a); // дискриминант равен нулю, корень один
+    return [x];
+} else {  
+  const x1 = (-b + Math.sqrt(d)) / (2 * a); // дискриминант больше нуля, два корня
+  const x2 = (-b - Math.sqrt(d)) / (2 * a); // вычисляем корни
+    return [x1, x2]; // возвращаем массив с двумя корнями
+  }
+} 
