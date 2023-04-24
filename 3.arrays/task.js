3d652a8caafed4f535fa3ae2b6d998b6ac22f03b
RinaK27 <103382676+RinaK27@users.noreply.github.com>
2023-04-24 21:21:53 +0400

    2

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,102 @@
 function compareArrays(arr1, arr2) {
-  
+	const equal = arr1.length === arr2.length && arr1.every((elem, index) => elem === arr2[index]);
+
+	return equal
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	const countGender = users.filter(i => i.gender === gender);
+	const avgAge = countGender.reduce(function(accumulator, currentValue) {
+		return accumulator + currentValue.age / (countGender.length || 1);
+	}, 0);
+
+	return avgAge;
+
+
 }
+
+const people = [{
+		firstName: "Александр",
+		secondName: "Карпов",
+		age: 17,
+		gender: "мужской"
+	},
+	{
+		firstName: "Егор",
+		secondName: "Морозов",
+		age: 21,
+		gender: "мужской"
+	},
+	{
+		firstName: "Мелисса",
+		secondName: "Леонова",
+		age: 40,
+		gender: "женский"
+	},
+	{
+		firstName: "Мелания",
+		secondName: "Савельева",
+		age: 37,
+		gender: "женский"
+	},
+	{
+		firstName: "Мария",
+		secondName: "Овчинникова",
+		age: 18,
+		gender: "женский"
+	},
+	{
+		firstName: "Марьяна",
+		secondName: "Котова",
+		age: 17,
+		gender: "женский"
+	},
+	{
+		firstName: "Фёдор",
+		secondName: "Селезнев",
+		age: 50,
+		gender: "мужской"
+	},
+	{
+		firstName: "Георгий",
+		secondName: "Петров",
+		age: 35,
+		gender: "мужской"
+	},
+	{
+		firstName: "Даниил",
+		secondName: "Андреев",
+		age: 49,
+		gender: "мужской"
+	},
+	{
+		firstName: "Дарья",
+		secondName: "Савельева",
+		age: 25,
+		gender: "женский"
+	},
+	{
+		firstName: "Михаил",
+		secondName: "Шаров",
+		age: 22,
+		gender: "мужской"
+	},
+	{
+		firstName: "Владислав",
+		secondName: "Давыдов",
+		age: 40,
+		gender: "мужской"
+	},
+	{
+		firstName: "Илья",
+		secondName: "Казаков",
+		age: 35,
+		gender: "мужской"
+	},
+	{
+		firstName: "Евгений",
+		secondName: "Кузьмин",
+		age: 19,
+		gender: "мужской"
+	},
+]
