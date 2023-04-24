f9b67fec59d72c9026d2d05ce0ee59a5a3c3b958
RinaK27 <103382676+RinaK27@users.noreply.github.com>
2023-04-24 21:25:37 +0400

    123

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,31 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
+}
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
+}
+Student.prototype.addMarks = function(...marksToAdd) {
+	if (this.hasOwnProperty('marks')) {
+		this.marks.push(...marksToAdd);
+	} else {
+		console.log("Студент отчислен, добавление оценок невозможно.")
+	}
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
-}
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.getAverage = function() {
+	if (this.marks && this.marks.length > 0) {
+		let sum = this.marks.reduce((acc, val) => acc + val);
+		return sum / this.marks.length;
+	} else {
+		return 0;
+	}
 }
-
-Student.prototype.getAverage = function () {
-  
-}
-
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
 }
